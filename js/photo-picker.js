e4dc1379c3cb31ba1657468c2cf63f080387c719
tthmatt <tthmatt@gmail.com>
2017-10-15 10:15:33 +0800

    Add files via upload

---
+++ js/photo-picker.js
@@ -0,0 +1,17 @@
+$('.ui.dropdown')
+  .dropdown()
+;$('.max.example .ui.normal.dropdown')
+  .dropdown({
+    maxSelections: 3
+  })
+;
+$('.max.example .ui.special.dropdown')
+  .dropdown({
+    useLabels: false,
+    maxSelections: 3
+  })
+;$('.tag.example .ui.dropdown')
+  .dropdown({
+    allowAdditions: true
+  })
+;
